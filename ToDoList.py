712ea1c13b0b62bd79af56a7ff6d64b0c9735008
Tan Jing Yu Denis <45563371+denistanjingyu@users.noreply.github.com>
2019-06-14 21:06:14 +0800

    Add files via upload

---
+++ ToDoList.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Import libraries
+import json
+import os
+
+
+# In[2]:
+
+
+#Open Json file and read the contents
+def getlist():
+    with open('C:\\Users\\user\\Desktop\\ToDo.json', "r") as file:
+        return json.load(file)
+
+
+# In[3]:
+
+
+#Function to add task input by user to list
+def addtasks(task):
+    to_Do_List = getlist()
+    to_Do_List.append(task.capitalize())
+    with open('C:\\Users\\user\\Desktop\\ToDo.json', "w") as file:
+        json.dump(to_Do_List, file)
+
+
+# In[4]:
+
+
+#Function to remove task number input by user from list
+def removetasks(num):
+    to_Do_List = getlist()
+    del to_Do_List[num-1]
+    with open('C:\\Users\\user\\Desktop\\ToDo.json', "w") as file:
+        json.dump(to_Do_List, file)
+
+
+# In[ ]:
+
+
+#Execute various commands
+#index+1 as python is zero indexed
+def main():
+    exit = ["q", "quit", "exit", "end"]
+    while True:
+        os.system('cls')
+        to_Do_List = getlist()
+        print("Tasks to Complete:")
+        print("------------------")
+        for index, todo in enumerate(to_Do_List):
+            print("{}) {}".format(index+1, todo))
+        
+        print("\nCommands: ")
+        print("Add tasks: ")
+        print("rm <#>\n")
+    
+        prompt = input("> ").strip().lower()
+        if prompt in exit:
+            break
+        elif prompt.startswith("add"):
+            task = prompt[4:]
+            addtasks(task)
+        elif prompt.startswith("rm"):
+            num = int(prompt[3:])
+            removetasks(num)
+main()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
